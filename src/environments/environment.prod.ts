883d49b9c7a2257ba73c88efb09c33d8d59b3169
ArMisae <52810384+ArMisae@users.noreply.github.com>
2019-07-13 02:13:36 -0600

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyChiVuqO7JaUpkjX1OYVzck58CXi87l9s8",
+    authDomain: "taskapp-49aae.firebaseapp.com",
+    databaseURL: "https://taskapp-49aae.firebaseio.com",
+    projectId: "taskapp-49aae",
+    storageBucket: "taskapp-49aae.appspot.com",
+    messagingSenderId: "177346478130",
+    appId: "1:177346478130:web:71f632af9e8ebdf3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyChiVuqO7JaUpkjX1OYVzck58CXi87l9s8",
+    authDomain: "taskapp-49aae.firebaseapp.com",
+    databaseURL: "https://taskapp-49aae.firebaseio.com",
+    projectId: "taskapp-49aae",
+    storageBucket: "taskapp-49aae.appspot.com",
+    messagingSenderId: "177346478130",
+    appId: "1:177346478130:web:71f632af9e8ebdf3"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
