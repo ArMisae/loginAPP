ac78b2acf56a5dabeb111da81dbc8b4280fa3746
ArMisae <52810384+ArMisae@users.noreply.github.com>
2019-07-12 20:19:08 -0600

    Creacion de Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    PrivatePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/private-page/private-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-page',
+  templateUrl: './private-page.component.html',
+  styleUrls: ['./private-page.component.scss']
+})
+export class PrivatePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
