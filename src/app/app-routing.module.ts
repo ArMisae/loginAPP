0cc507044c2a298a1c61d49c9406522ab0255e45
ArMisae <52810384+ArMisae@users.noreply.github.com>
2019-07-12 21:31:19 -0600

    Añadir el header y las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PrivatePageComponent } from './components/private-page/private-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path:'register', component: RegisterPageComponent},
+  {path:'private', component: PrivatePageComponent},
+  {path:'notfound', component: NotFoundPageComponent},
+  {path:'', component: HomePageComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
